2ba73aad0ccdee7c352c39f94cb4f9fc941535a6
Henry Wong <henrywong723@hotmail.com>
2021-05-01 20:11:41 -0400

    Added items to HTML Add all items from list to HTML with tags and price formatting

--- src/index.js
+++ src/index.js
@@ -26,7 +26,69 @@ starterItems.sort(leastToGreatest);
 pastaItems.sort(leastToGreatest);
 pizzaItems.sort(leastToGreatest);
 
-function itemsToHTML(listOfItems, idName) {}
+itemsToHTML(starterItems, "starters", "item-list");
+itemsToHTML(pastaItems, "pasta", "item-list");
+itemsToHTML(pizzaItems, "pizza", "item-list");
+
+/*
+<dl class="item-list">
+<dt>sds</dt>
+<dd>sdf</dd>
+</dl>
+*/
+
+function itemsToHTML(listOfItems, idName, className) {
+  //Only add items to the list if they exist
+  if (listOfItems.length !== 0 || listOfItems !== undefined) {
+    //Get the section by id
+    var section = document.getElementById(idName);
+
+    //add a dl tag
+    var item = "<dl class=" + className + ">";
+
+    //adds all the items from their respective item list
+    for (var i = 0; i < listOfItems.length; i++) {
+      var itemInfo;
+
+      //add the spicy class if spicy
+      if (listOfItems[i].spicy) {
+        itemInfo = '<dt class="disclaimer spicy">';
+      } else {
+        itemInfo = "<dt>";
+      }
+
+      //adds the rest of the info of item
+      itemInfo +=
+        listOfItems[i].name +
+        " " +
+        formatPrice(listOfItems[i].price) +
+        "</dt><dd>" +
+        listOfItems[i].description +
+        "</dd>";
+      item += itemInfo;
+    }
+    item += "</dl>";
+    section.innerHTML += item;
+    console.log(section);
+  }
+}
+
+function formatPrice(price) {
+  var priceString = price.toString();
+  var priceArray = priceString.split("");
+  var indexOfPeriod = priceArray.indexOf(".");
+
+  /* The space between the length of array
+   * and index of the period to determine
+   * if there is two trailing numbers
+   */
+  if (priceArray.length - indexOfPeriod === 2) {
+    priceArray.push("0");
+  }
+  priceArray.unshift("$");
+  var formattedPrice = priceArray.join("");
+  return formattedPrice;
+}
 
 //Sorting function order from least to greatest
 function leastToGreatest(a, b) {
