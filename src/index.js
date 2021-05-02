36b74956369d589f84ddabda06d57b74055ff8ad
Henry Wong <henrywong723@hotmail.com>
2021-05-02 04:38:18 -0400

    Added checkbox to exclude spicy items Checkbox to show only spicy items is checked and updates the webpage

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import "./styles.css";
 import menu from "./menu";
 
 const menuItems = menu.items;
+const numberOfMenuTypes = 3;
 var starterItems = [];
 var pastaItems = [];
 var pizzaItems = [];
-
-console.log("NEW ITERATION");
 
 //Splits the entire list of items into separate lists of each respective type
 for (var i = 0; i < menuItems.length; i++) {
@@ -26,47 +25,74 @@ starterItems.sort(leastToGreatest);
 pastaItems.sort(leastToGreatest);
 pizzaItems.sort(leastToGreatest);
 
+//Runs when page load with the spicy items checked
 itemsToHTML(starterItems, "starters", "item-list");
 itemsToHTML(pastaItems, "pasta", "item-list");
 itemsToHTML(pizzaItems, "pizza", "item-list");
 
+//event listener for checkbox changes to update the menu
+document
+  .getElementById("spicy checkbox")
+  .addEventListener("change", function () {
+    var spicyCheck = document.getElementById("spicy checkbox").checked;
+    updateMenu(spicyCheck);
+  });
+
+/* function that removes the current list of items then
+ * re-adds it with an updated version of the list of items
+ * depending if the checkbox is checked or not
+ */
+function updateMenu(spicyCheck) {
+  //remove all the list of items on that are in the HTML
+  for (var i = 0; i < numberOfMenuTypes; i++) {
+    var thisList = document.getElementById("item-list");
+    thisList.remove();
+  }
+  //re-add the items to the HTML with the correct spicy/non-spicy items shown
+  itemsToHTML(starterItems, "starters", "item-list");
+  itemsToHTML(pastaItems, "pasta", "item-list");
+  itemsToHTML(pizzaItems, "pizza", "item-list");
+}
+
 //function that add items from list to the HTML
-function itemsToHTML(listOfItems, idName, className) {
-  //Only add items to the list if they exist
+function itemsToHTML(listOfItems, sectionIdName, listIdName) {
+  //Only add items to the list if they exist in the list
   if (listOfItems.length !== 0 || listOfItems !== undefined) {
-    //Get the section by id
-    var section = document.getElementById(idName);
-
-    //add a dl tag
-    var item = "<dl class=" + className + ">";
-
+    var section = document.getElementById(sectionIdName);
+    var spicyCheck = document.getElementById("spicy checkbox").checked;
+    var item = "<dl id=" + listIdName + ">";
     //adds all the items from their respective item list
     for (var i = 0; i < listOfItems.length; i++) {
       var itemInfo;
-
       //add the spicy class if spicy
-      if (listOfItems[i].spicy) {
-        itemInfo = '<dt class="disclaimer spicy">';
-      } else {
-        itemInfo = "<dt>";
+      if (listOfItems[i].spicy && spicyCheck) {
+        itemInfo =
+          '<dt class="disclaimer spicy">' +
+          listOfItems[i].name +
+          " " +
+          formatPrice(listOfItems[i].price) +
+          "</dt><dd>" +
+          listOfItems[i].description +
+          "</dd>";
+        item += itemInfo;
+      } else if (!listOfItems[i].spicy) {
+        itemInfo =
+          "<dt>" +
+          listOfItems[i].name +
+          " " +
+          formatPrice(listOfItems[i].price) +
+          "</dt><dd>" +
+          listOfItems[i].description +
+          "</dd>";
+        item += itemInfo;
       }
-
-      //adds the rest of the info of item
-      itemInfo +=
-        listOfItems[i].name +
-        " " +
-        formatPrice(listOfItems[i].price) +
-        "</dt><dd>" +
-        listOfItems[i].description +
-        "</dd>";
-      item += itemInfo;
     }
     item += "</dl>";
     section.innerHTML += item;
-    console.log(section);
   }
 }
 
+//function to format the price properly
 function formatPrice(price) {
   var priceString = price.toString();
   var priceArray = priceString.split("");
